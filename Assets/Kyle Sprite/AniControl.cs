b5b80ae6788e979e7b9eb2207b154553ed659a6f
James Bassett <BassettJB@mukilteo.wednet.edu>
2018-11-30 21:27:25 -0800

    This commit shows a variety of tricks for 2D.

---
+++ Assets/Kyle Sprite/AniControl.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AniControl : MonoBehaviour {
+
+    public float ctlSpeed;
+    Animator ani;
+    Vector3 v3;
+    float scaleX;
+    bool jumpState = false;
+
+	// Use this for initialization
+	void Start () {
+        ani = GetComponent<Animator>();
+        v3 = transform.localScale;
+        scaleX = v3.x;
+        Debug.Log(scaleX);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+        ctlSpeed = Input.GetAxis("Horizontal");
+        ani.SetFloat("aniCtlSpeed", ctlSpeed);
+        if (ctlSpeed > 0) v3.x = -scaleX;
+        if (ctlSpeed < 0) v3.x = scaleX;
+      
+        if (Input.GetKeyDown("space"))
+        {
+            jumpState = !jumpState;
+            
+            ani.SetBool("aniJump", jumpState);
+            if (jumpState)
+            {
+                //v3.y = -1;
+               
+            } else
+            {
+                v3.y = 1;
+            }
+        }
+        transform.localScale = v3;
+
+
+    }
+}
